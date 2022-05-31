6e22b896e92af6bbf79e1b8b6a834cb9ccfa0c2e
Rhababaer <alex.no.94@gmail.com>
2022-05-31 02:07:57 +0200

    nothing major

--- arduino.sh
+++ arduino.sh
@@ -1,27 +1,27 @@
 #!/bin/sh
 
-BLUE='\033[0;92m'
+BLUE='\033[1;33m'
 NC='\033[0m'
 
-echo -e ${BLUE}install Arduino IDE${NC}
+echo -e "${BLUE}(1/5) install Arduino IDE${NC}"
 sudo apt install arduino
 
-echo -e ${BLUE}get Arduino dark theme${NC}
+echo -e "${BLUE}(2/5) get Arduino dark theme${NC}"
 wget https://github.com/konrad91/OneDarkArduino/tree/master/theme
 
 sudo mv /usr/share/arduino/lib/theme /usr/share/arduino/lib/theme_backup
 sudo mv theme /usr/share/arduino/lib/
 
-echo -e ${BLUE}get cli interface${NC}
+echo -e "${BLUE}(3/5) get cli interface${NC}"
 curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh |sh
 
-echo -e ${BLUE}add esp8266 and esp32 to boards$ resources{NC}
+echo -e "${BLUE}(4/5) add esp8266 and esp32 to boards$ resources{NC}"
 echo boardsmanager.additional.urls=http://arduino.esp8266.com/stable/package_esp8266com_index.json, https://dl.espressif.com/dl/package_esp32_index.json >> .arduino15/preferences.txt
 arduino-cli core update-index
 arduino-cli core install esp32:esp32 --additional-urls "https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json"
 arduino-cli core install esp8266:esp8266 --additional-urls "http://arduino.esp8266.com/stable/package_esp8266com_index.json"
 
-echo -e ${BLUE}install libraries for MAX31856, INA219 and MCP4725${NC}
+echo -e "${BLUE}(5/5) install libraries for MAX31856, INA219 and MCP4725${NC}"
 arduino-cli lib install "Adafruit MAX31856 library"
 arduino-cli lib install "Adafruit INA219"
 arduino-cli lib install "Adafruit MCP4725"
--- python3.10.sh
+++ python3.10.sh
@@ -1,21 +1,32 @@
 #!/bin/sh
 
-BLUE='\033[0;92m'
+BLUE='\033[1;33m'
 NC='\033[0m'
 
-echo -e ${BLUE}Download Python 3.10.2${NC}
+echo -e "${BLUE}(1/6) Download Python 3.10.2${NC}"
 wget https://www.python.org/ftp/python/3.10.2/Python-3.10.2.tgz
 
-echo -e ${BLUE}unzip files${NC}
+echo -e "${BLUE}(2/6) unzip files${NC}"
 tar -zxvf Python-3.10.2.tgz
 
-echo -e ${BLUE}clear dependencies${NC}
+echo -e "${BLUE}(3/6)clear dependencies${NC}"
 sudo apt-get install build-essential gdb lcov pkg-config libbz2-dev libffi-dev libgdbm-dev libgdbm-compat-dev liblzma-dev libncurses5-dev libreadline6-dev libsqlite3-dev libssl-dev lzma lzma-dev tk-dev uuid-dev zlib1g-dev
 
-echo -e ${BLUE}set configuration parameters${NC}
+echo -e "${BLUE}(4/6) set configuration parameters${NC}"
  cd /Python-3.10.2
  ./configure --enable-optimizations --with-openssl="/usr" --libdir=/usr/local/lib
 
-echo -e ${BLUE}install Python 3.10.2${NC}
+ echo -e "${BLUE}(5/6) install Python 3.10.2${NC}"
 sudo make altinstall
-cd
+
+ echo -e "${BLUE}(6/6) install libraries${NC}"
+ pip3.10 install appJar
+ pip3 install appJar
+ pip install appJar
+ pip3.10 install pySerial
+ pip3 install pySerial
+ pip install pySerial
+ pip3.10 install pyInstaller
+ pip3 install pyInstaller
+ pip install pyInstaller
+ pip3.10 install RPi.GPIO
