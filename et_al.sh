b676603ee42a351cc3d2f69ed066b9dceaf410c8
Rhababaer <60236059+Rhababaer@users.noreply.github.com>
2022-08-23 18:23:54 +0200

    Update et_al.sh

--- et_al.sh
+++ et_al.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+sudo apt update
+sudo apt upgrade
+sudo apt install vim
+
 cd ~
 git clone https://github.com/th33xitus/kiauh.git
 git clone https://github.com/mainsail-crew/moonraker-timelapse.git
